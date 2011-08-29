0923da8fd360015443e1d26776a485117ad4f552
Amro Mousa <amromousa@gmail.com>
2011-08-29 14:27:36 -0400

    Proper fix for NoMethodError settings

--- lib/handlers/uakari_delivery_handler.rb
+++ lib/handlers/uakari_delivery_handler.rb
@@ -1,29 +1,27 @@
 require 'action_mailer'
 
 class UakariDeliveryHandler
+  attr_accessor :settings
+  
   def initialize options
     @uakari = Uakari.new(options[:apikey])
-    @options = {:track_opens => true, :track_clicks => true, :tags => []}.merge(options)
+    self.settings = {:track_opens => true, :track_clicks => true}.merge(options)
   end
   
   def deliver! message
     @uakari.send_email({
-     :track_opens => @options[:track_opens],
-     :track_clicks => @options[:track_clicks],
+     :track_opens => settings[:track_opens],
+     :track_clicks => settings[:track_clicks],
      :message => {
        :subject => message.subject,
        :html => message.text_part.body,
        :text => message.html_part.body,
-       :from_name => @options[:from_name],
+       :from_name => settings[:from_name],
        :from_email => message.from.first,
        :to_email => message.to
-     },
-     :tags => (message[:tags] || @options[:tags])
+     }
     })
   end
 
-  def settings
-    @options
-  end
 end
 ActionMailer::Base.add_delivery_method :uakari, UakariDeliveryHandler
