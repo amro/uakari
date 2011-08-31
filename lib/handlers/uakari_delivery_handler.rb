164dc52cb0eb5db3593ad803e6c3f83903a466d0
Stafford Brooke <stafford@highgroove.com>
2011-08-31 18:48:27 -0400

    [#17712725] inital spike on a refactor

--- lib/handlers/uakari_delivery_handler.rb
+++ lib/handlers/uakari_delivery_handler.rb
@@ -1,22 +1,19 @@
-class UakariDeliveryHandler
-  def initialize options
-    @uakari = Uakari.new(options[:apikey])
-    @options = {:track_opens => true, :track_clicks => true}.merge(options)
-  end
-  
-  def deliver! message
-    @uakari.send_email({
-     :track_opens => @options[:track_opens],
-     :track_clicks => @options[:track_clicks],
-     :message => {
-       :subject => message.subject,
-       :html => message.text_part.body,
-       :text => message.html_part.body,
-       :from_name => @options[:from_name],
-       :from_email => message.from.first,
-       :to_email => message.to
-     }
-    })
+module Uakari
+  class DeliveryHandler
+    def deliver! message
+     self.send_email({
+       :track_opens => @options[:track_opens],
+       :track_clicks => @options[:track_clicks],
+       :message => {
+         :subject => message.subject,
+         :html => message.text_part.body,
+         :text => message.html_part.body,
+         :from_name => @options[:from_name],
+         :from_email => message.from.first,
+         :to_email => message.to
+       }
+      })
+    end
   end
 end
-ActionMailer::Base.add_delivery_method :uakari, UakariDeliveryHandler
+ActionMailer::Base.add_delivery_method :uakari, Uakari::DeliveryHandler
--- lib/uakari.rb
+++ lib/uakari.rb
@@ -2,51 +2,46 @@ require 'httparty'
 require 'json'
 require 'cgi'
 require File.join(File.dirname(__FILE__), 'handlers', 'uakari_delivery_handler')
+module Uakari
+  class << self
+    include HTTParty
+    default_timeout 30
 
-class Uakari
-  include HTTParty
-  default_timeout 30
-
-  attr_accessor :apikey, :timeout, :options
-
-  def initialize(apikey = nil, extra_params = {})
-    @apikey = apikey
-    @default_params = {
-      :apikey => apikey,
-      :options => {
-        :track_opens => true, 
-        :track_clicks => true
-      }
-    }.merge(extra_params)
-  end
-
-  def apikey=(value)
-    @apikey = value
-    @default_params = @default_params.merge({:apikey => @apikey})
-  end
-
-  def base_api_url
-    dc = @apikey.blank? ? '' : "#{@apikey.split("-").last}."
-    "https://#{dc}sts.mailchimp.com/1.0/"
-  end
-
-  def call(method, params = {})
-    url = "#{base_api_url}#{method}"
-    params = @default_params.merge(params)
-    response = self.class.post(url, :body => params, :timeout => @timeout)
-
-    begin
-      response = JSON.parse(response.body)
-    rescue
-      response = response.body
+    attr_accessor :apikey, :timeout, :options
+    
+    def base_api_url
+      dc = self.apikey.blank? ? '' : "#{self.apikey.split("-").last}."
+      "https://#{dc}sts.mailchimp.com/1.0/"
     end
-    response
-  end
 
-  def method_missing(method, *args)
-    method = method.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase } #Thanks for the gsub, Rails
-    args = {} unless args.length > 0
-    args = args[0] if (args.class.to_s == "Array")
-    call(method, args)
+    def call(method, params = {})
+      url = "#{base_api_url}#{method}"
+      params = self.default_params.merge(params)
+      response = self.class.post(url, :body => params, :timeout => self.timeout)
+
+      begin
+        response = JSON.parse(response.body)
+      rescue
+        response = response.body
+      end
+      response
+    end
+    
+    def default_params(extra_params = {})
+      {
+        :apikey => self.apikey,
+        :options => {
+          :track_opens => true, 
+          :track_clicks => true
+        }
+      }.merge(extra_params)
+    end
+
+    def method_missing(method, *args)
+      method = method.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase } #Thanks for the gsub, Rails
+      args = {} unless args.length > 0
+      args = args[0] if (args.class.to_s == "Array")
+      call(method, args)
+    end
   end
 end
